2a3c03e612de8be435259f30f5708abe6f5bbb53
correalo <82238324+correalo@users.noreply.github.com>
2021-08-10 20:53:43 -0300

    implementação da rota do Login

--- routers/marca.routes.js
+++ routers/marca.routes.js
@@ -1,8 +1,9 @@
 const { Router } = require('express')
 const router = Router()
-const marcaController = require('../controllers/marca.controller')
+const marcaController = require('../controllers/marca.controller');
+const auth = require('../middlewares/auth');
 
-router.get('/marca/:id', marcaController.marca);
+router.get('/marca/:id',auth, marcaController.marca);
 
 
 module.exports = router

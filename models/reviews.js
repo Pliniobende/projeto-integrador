8f450de568c35dcdf5166c3858dd1f9fe9362301
GregPinheiro <gpinheirojunior@gmail.com>
2021-09-12 14:05:57 -0300

    Load table reviews

---
+++ models/reviews.js
@@ -0,0 +1,47 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Reviews extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Reviews.belongsTo(models.User, {
+        foreignKey: userId,
+        as: 'User',
+        require: true
+      }),
+      Reviews.belongsTo(models.Posts, {
+        foreignKey: postId,
+        as: 'Post',
+        require: true
+      })
+    }
+  };
+  Reviews.init({
+    rating: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
+    description: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    userId: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
+    postId: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    }
+  }, {
+    sequelize,
+    modelName: 'Reviews',
+  });
+  return Reviews;
+};
